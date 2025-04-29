ae3476e906d77d6bfd16880f625908472ff402df
Jefferson Ferreira <Jefferson1489@gmail.com>
2025-04-29 10:56:20 -0300

    Initial Commit

---
+++ src/app/components/player/player.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player',
+  imports: [],
+  templateUrl: './player.component.html',
+  styleUrl: './player.component.scss'
+})
+export class PlayerComponent {
+
+}
